096e245950fb3747bcaa237068fd4bb5723d7974
Areef shaik <areef.shaik@prasthana.com>
2026-04-02 17:40:12 +0530

    updated changes

--- scientific_calculator/test/widget_test.dart
+++ scientific_calculator/test/widget_test.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:scientific_calculator/main.dart';
-import 'package:scientific_calculator/features/calculator/calculator_view.dart';
 import 'package:scientific_calculator/features/calculator/widgets/calculator_button.dart';
 
 void main() {
